a948881628b62f9d9c0a9224d856b5f1ef7e617a
Lior Banai <liorb@kama-medical.com>
2020-09-13 10:11:09 +0300

    nuget update

--- Analogy.LogViewer.VisualStudioActivityLog/IAnalogy/VSComponentImages.cs
+++ Analogy.LogViewer.VisualStudioActivityLog/IAnalogy/VSComponentImages.cs
@@ -5,28 +5,28 @@ using Analogy.LogViewer.VisualStudioActivityLog.Properties;
 
 namespace Analogy.LogViewer.VisualStudioActivityLog.IAnalogy
 {
-    public class VSComponentImages : IAnalogyComponentImages
-    {
-        public Image GetLargeImage(Guid analogyComponentId)
-        {
-            if (analogyComponentId == VSActivityLogFactory.factory)
-                return Resources.AnalogyVS32x32;
-            return null;
-        }
+    //public class VSComponentImages : IAnalogyComponentImages
+    //{
+    //    public Image GetLargeImage(Guid analogyComponentId)
+    //    {
+    //        if (analogyComponentId == VSActivityLogFactory.factory)
+    //            return Resources.AnalogyVS32x32;
+    //        return null;
+    //    }
 
-        public Image GetSmallImage(Guid analogyComponentId)
-        {
-            if (analogyComponentId == VSActivityLogFactory.factory)
-                return Resources.VS16x16;
-            return null;
-        }
+    //    public Image GetSmallImage(Guid analogyComponentId)
+    //    {
+    //        if (analogyComponentId == VSActivityLogFactory.factory)
+    //            return Resources.VS16x16;
+    //        return null;
+    //    }
 
-        public Image GetOnlineConnectedLargeImage(Guid analogyComponentId) => null;
+    //    public Image GetOnlineConnectedLargeImage(Guid analogyComponentId) => null;
 
-        public Image GetOnlineConnectedSmallImage(Guid analogyComponentId) => null;
+    //    public Image GetOnlineConnectedSmallImage(Guid analogyComponentId) => null;
 
-        public Image GetOnlineDisconnectedLargeImage(Guid analogyComponentId) => null;
+    //    public Image GetOnlineDisconnectedLargeImage(Guid analogyComponentId) => null;
 
-        public Image GetOnlineDisconnectedSmallImage(Guid analogyComponentId) => null;
-    }
+    //    public Image GetOnlineDisconnectedSmallImage(Guid analogyComponentId) => null;
+    //}
 }
